73367b8faf1fe9b4228acafff6872e66d6b0997b
Arina Pavlova <apavlova05@mail.ru>
2026-06-16 17:05:17 +0300

    fix error status codes for json parse and type mismatch errors

--- menu/src/main/java/com/example/menu/exception/GlobalExceptionHandler.java
+++ menu/src/main/java/com/example/menu/exception/GlobalExceptionHandler.java
@@ -3,13 +3,31 @@ package com.example.menu.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Невалидный JSON
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleJsonParseError(HttpMessageNotReadableException ex) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Некорректный формат JSON в теле запроса");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    // Неправильный тип параметра
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String message = "Параметр '" + ex.getName() + "' должен быть числом";
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 
     // Обрабатывает нарушение уникальности
     @ExceptionHandler(DataIntegrityViolationException.class)
