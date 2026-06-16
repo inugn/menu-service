a1b165693822ea395106a76ffb571dedf74c361b
Arina Pavlova <apavlova05@mail.ru>
2026-06-16 12:08:32 +0300

    add healthcheck in docker-compose.yml

--- menu/src/main/java/com/example/menu/model/Dish.java
+++ menu/src/main/java/com/example/menu/model/Dish.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 
+
 @Entity
 @Table(name = "dish")
 public class Dish {
