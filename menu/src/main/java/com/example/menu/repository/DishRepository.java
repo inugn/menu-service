67295a73dd7caddf22e96b72ade1b5fddaa8e7a6
Arina Pavlova <apavlova05@mail.ru>
2026-06-18 17:30:14 +0300

    update DishRepository

--- menu/src/main/java/com/example/menu/repository/DishRepository.java
+++ menu/src/main/java/com/example/menu/repository/DishRepository.java
@@ -1,14 +1,14 @@
 package com.example.menu.repository;
 
-import com.example.menu.model.Dish;
+import  com.example.menu.model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface DishRepository extends JpaRepository<Dish, Long> {
-    @Query("SELECT d FROM Dish d JOIN FETCH d.category")
+    @Query("SELECT DISTINCT d FROM Dish d JOIN FETCH d.category LEFT JOIN FETCH d.ingredients")
     List<Dish> findAllWithCategory();
 
-    @Query("SELECT d FROM Dish d JOIN FETCH d.category WHERE d.category.id = :categoryId")
+    @Query("SELECT DISTINCT d FROM Dish d JOIN FETCH d.category LEFT JOIN FETCH d.ingredients WHERE d.category.id = :categoryId")
     List<Dish> findByCategoryId(Long categoryId);
 }
