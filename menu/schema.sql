05b468995678c10aaaaa9ad7c269ca5cf02c52e8
Arina Pavlova <apavlova05@mail.ru>
2026-06-18 17:31:16 +0300

    update schema.sql

--- menu/schema.sql
+++ menu/schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE dish (
     category_id BIGINT NOT NULL,
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES menu_category(id)
 );
+
+CREATE TABLE ingredient (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE dish_ingredient (
+    dish_id BIGINT NOT NULL,
+    ingredient_id BIGINT NOT NULL,
+    PRIMARY KEY (dish_id, ingredient_id),
+    CONSTRAINT fk_dish FOREIGN KEY (dish_id) REFERENCES dish(id),
+    CONSTRAINT fk_ingredient FOREIGN KEY (ingredient_id) REFERENCES ingredient(id)
+);
